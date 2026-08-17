9ee0fb64866f61877caa6797ca2812521c09297f
Abhinav Karnatak <abhinavkarnatak2004@gmail.com>
2026-08-17 09:41:03 +0530

    Madeup syntax error to check Nimbus

--- stringFormatting.py
+++ stringFormatting.py
@@ -5,4 +5,4 @@ print("We all are {}".format("equal"))
 print("{2} {1} {3} {0}".format("Abhinav","name", "My", "is"))
 
 my_job = "AI Engineer"
-print(f"I am a future {my_job}")
+printf(f"I am a future {my_job}")
